6cf1d3e04fd197b98d6c721729ba89b2e6ce03ef
Devan Bhensdadiya <92723752+devanpatel28@users.noreply.github.com>
2024-02-22 10:03:32 +0530

    Server Changes

--- server.js
+++ server.js
@@ -3,9 +3,11 @@ const category = require('./src/category/category.routes')
 const product = require('./src/product/product.routes')
 const variant = require('./src/variant/variant.routes')
 const errors = require("./src/middleware/error")
+const cors = require('cors')
 const app = express()
 const port = 3000
 
+app.use(cors())
 app.use(express.json())
 app.use(errors.errorHandler)
 app.use('/category',category);
@@ -13,4 +15,4 @@ app.use('/product',product);
 app.use('/variant',variant);
 
 app.get('/', (req, res) => res.send('Hello World!'))
-app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+app.listen(port, () => console.log(`Example app listening on port ${port}!`))   
